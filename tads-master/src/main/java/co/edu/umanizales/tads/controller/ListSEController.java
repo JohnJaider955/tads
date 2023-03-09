f2f4afc465929af0c41d39fc7e276115f60a65b5
JohnJaider955 <127434052+JohnJaider955@users.noreply.github.com>
2023-03-09 13:38:36 -0500

    Add files via upload

---
+++ tads-master/src/main/java/co/edu/umanizales/tads/controller/ListSEController.java
@@ -0,0 +1,33 @@
+package co.edu.umanizales.tads.controller;
+
+import co.edu.umanizales.tads.controller.dto.ResponseDTO;
+import co.edu.umanizales.tads.model.Kid;
+import co.edu.umanizales.tads.model.ListSE;
+import co.edu.umanizales.tads.service.ListSEService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/listse")
+public class ListSEController {
+    @Autowired
+    private ListSEService listSEService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO> getKids(){
+        return new ResponseEntity<>(new ResponseDTO(
+                200,listSEService.getKids(),null), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/delete/{code}")
+    public ResponseEntity<ResponseDTO> deleteKids(
+            @PathVariable String identification){
+        return new ResponseEntity<>(new ResponseDTO(200,
+                "El niño ha sido eliminado."+listSEService.getKidIdentification(identification), null),
+                HttpStatus.OK);
+    }
+
+}
+
